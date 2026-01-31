9ea3e359d86b0aef7bc63521fa176ad86fc9cf32
karmartir <karmartir@gmail.com>
2026-01-31 11:53:54 -0500

    Add messages and styling options to StarRating component

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,16 @@ import StarRating from "./StarRating.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["terrible", "bad", "ok", "good", "great"]}
+    />
+    <StarRating
+      maxRating={10}
+      color="red"
+      size={24}
+      className="test"
+      defaultRating={1}
+    />
   </StrictMode>,
 );
