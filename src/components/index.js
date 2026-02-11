7c761fc832aa68c59a500e5e82ff72ff38d28632
karmartir <karmartir@gmail.com>
2026-02-10 22:45:48 -0500

    fixed errors in index and rename component

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,11 @@
 // src/components/index.js
-export { default as NavBar } from "./Navbar";
+export { default as NavBar } from "./NavBar";
 export { default as NumResults } from "./NumResults";
 export { default as Search } from "./Search";
 export { default as MovieDetails } from "./MovieDetails";
 export { default as MovieList } from "./MovieList";
 export { default as WatchedSummary } from "./WatchedSummary";
-export { default as WatchedMoviesList } from "./WatchedMovieList";
+export { default as WatchedMoviesList } from "./WatchedMoviesList";
 export { default as Box } from "./Box";
 export { default as Main } from "./Main";
 export { default as Loader } from "./Loader";
